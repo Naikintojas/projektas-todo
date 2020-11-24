34512401f9f192e5d699c88dde35d97ba6e1a563
Naikintojas <rokgytis@gmail.com>
2020-11-24 14:33:39 +0200

    update

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,9 +2,15 @@
 
 @section('content')
     <div class="container mt-5">
-        <p class="h3" id="invoice" >{{$post->title}} {{$post->aprasymas}} {{$post->specifikacija}} {{$post->moduliai}} {{$post->extra}}</p>
-        <small><p class="h5 d-inline" id="invoice">It was created at:</p> {{$post->created_at}}</small>
+        <div id="invoice">
+        <p class="h3" ><b>Project Name:</b> {{$post->title}} {{$post->extra}}</p>
+        <p class="h3" ><b>Description:</b> {{$post->aprasymas}}</p>
+        <p class="h3" ><b>Specification:</b> {{$post->specifikacija}}</p>
+        <p class="h3" ><b>Moduels:</b> {{$post->moduliai}}</p>
+        <p class="h3" ><b>More info:</b> {{$post->extra}}</p>
+        <small><p class="h5 d-inline">It was created at:</p> {{$post->created_at}}</small>
         <hr>
+        </div>
     <a href="/posts/{{$post->id}}/edit" class="btn btn-warning mt-3">Edit</a>
     <form action="{{action('App\Http\Controllers\PostsController@destroy', $post['id'])}}" method="POST" class="d-inline ">
         {{ csrf_field() }}
